ebc9ece6ff51b1ce27fbc36d041f195b2c30cc1a
Maxendre Buvot <maxendre.buvot@mines-paristech.fr>
2021-03-17 00:24:50 +0100

    preview photo + bouton reprendre photo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 
 const styles = StyleSheet.create({
@@ -58,6 +58,58 @@ export default function App() {
     })();
   }, []);
 
+  const retakePicture = () => {
+    setCapturedImage(null);
+    setPreviewVisible(false);
+  };
+
+  const CameraPreview = ({ photo }) => {
+    console.log('sdsfds', photo)
+    return (
+      <View
+        style={{
+          backgroundColor: 'transparent',
+          flex: 1,
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <ImageBackground
+          source={{ uri: photo && photo.uri }}
+          style={{
+            flex: 1,
+          }}
+        />
+        <TouchableOpacity
+          onPress={retakePicture}
+          style={{
+            width: 100,
+            height: 40,
+            position: 'absolute',
+            alignItems: 'center',
+            borderRadius: 10,
+            bottom: 0,
+            borderWidth: 3,
+            alignContent: 'center',
+            justifyContent: 'center',
+            borderColor: 'white',
+            marginBottom: 20,
+            alignSelf: 'center'
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontSize: 20
+            }}
+          >
+            Re-take
+            </Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   if (hasPermission === null) {
     return <View />;
   }
@@ -66,31 +118,35 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Camera ref={ref => { camera = ref; }} style={styles.camera} type={type}>
-        <View style={styles.buttonFlip}>
+      {previewVisible && capturedImage ? (
+        <CameraPreview photo={capturedImage} />
+      ) : (
+        <Camera ref={ref => { camera = ref; }} style={styles.camera} type={type}>
+          <View style={styles.buttonFlip}>
+            <TouchableOpacity
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}>
+              <Text style={styles.text}> Flip </Text>
+            </TouchableOpacity>
+          </View>
           <TouchableOpacity
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
+            style={styles.buttonPicture}
+            onPress={async () => {
+              if (camera) {
+                let photo = await camera.takePictureAsync()
+                console.log(photo)
+                setPreviewVisible(true)
+                setCapturedImage(photo)
+              }
             }}>
-            <Text style={styles.text}> Flip </Text>
           </TouchableOpacity>
-        </View>
-        <TouchableOpacity
-          style={styles.buttonPicture}
-          onPress={async () => {
-            if (camera) {
-              let photo = await camera.takePictureAsync()
-              console.log(photo)
-              setPreviewVisible(true)
-              setCapturedImage(photo)
-            }
-          }}>
-        </TouchableOpacity>
-      </Camera>
+        </Camera>
+      )}
     </View>
   );
 }
